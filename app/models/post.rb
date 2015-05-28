c6256738ecae51d28b19749eef139bd7ebe5bce7
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2015-05-28 05:12:01 +0200

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ActiveRecord::Base
+	has_attached_file :avatar, 
+:path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
+:url => "/system/:attachment/:id/:basename_:style.:extension",
+:styles => {
+  :thumb    => ['100x100#',  :jpg, :quality => 70],
+  :preview  => ['480x480#',  :jpg, :quality => 70],
+  :original    => ['100x720^',      :jpg, :quality => 70]
+  
+},
+:convert_options => {
+  :thumb    => '-set colorspace sRGB -strip',
+  :preview  => '-set colorspace sRGB -strip',
+  :original   => '-set colorspace sRGB -strip'
+}
+validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+end
