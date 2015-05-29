f9da28b08c4e7a6d35bf34aad4819ccf82b799ba
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2015-05-29 03:00:47 +0200

    added admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+newadmin = Admin.new({:email => 'arifgoemleksiz101@gmail.com', :password=> 'qwe123qwe',:password_confirmation => 'qwe123qwe'})
+
+newadmin.save
+newuser = User.new({:name=> 'ataraxie67', :email => 'arifgoemleksiz101@gmail.com', :password=> 'qwe123qwe',:password_confirmation => 'qwe123qwe'})
+
+newuser.save
+newuser1 = User.new({:name=> 'ataraxie68', :email => 'arifgoemleksiz102@gmail.com', :password=> 'qwe123qwe',:password_confirmation => 'qwe123qwe'})
+
+newuser1.save
+newuser2 = User.new({:name=> 'ataraxie69', :email => 'arifgoemleksiz103@gmail.com', :password=> 'qwe123qwe',:password_confirmation => 'qwe123qwe'})
+
+newuser2.save
+newuser3 = User.new({:name=> 'ataraxie70', :email => 'arifgoemleksiz104@gmail.com', :password=> 'qwe123qwe',:password_confirmation => 'qwe123qwe'})
+
+newuser3.save
